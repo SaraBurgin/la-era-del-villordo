dbc5d83dd631eb98bf2080e5ae6c947265c79c09
Sara <saragonzalezburgin@hotmail.com>
2020-11-20 16:04:09 +0100

    Add responsiveness to home page

--- components/ControlledCarousel.js
+++ components/ControlledCarousel.js
@@ -10,43 +10,49 @@ export default function ControlledCarousel() {
 
   return (
     <div className="carousel-container">
-      <img src="/images/background-design.png" className="background-left-side"/>
         <Carousel className="carousel-first" activeIndex={index} onSelect={handleSelect}>
+
           <Carousel.Item>
             <img
               className="d-block w-100 img-swimmingpool"
               src="/images/la-era-piscina.jpg"
               alt="First slide"
-    
             />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Swimming Pool.</p>
-            </Carousel.Caption>
           </Carousel.Item>
+          
           <Carousel.Item>
             <img
               className="d-block w-100 img-exterior"
               src="/images/la-era-exterior.jpg"
-              alt="Second slide"
+              alt="exterior-img"
             />
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Exterior</p>
-            </Carousel.Caption>
           </Carousel.Item>
+
           <Carousel.Item>
             <img
               className="d-block w-100 img-master-bedroom"
               src="/images/la-era-master-bedroom.jpg"
+              alt="master-bedroom-img"
             />
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Master Bedroom</p>
-            </Carousel.Caption>
           </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100 img-barbacue"
+              src="/images/barbacoa.jpg"
+              alt="barbacue-img"
+            />
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100 img-pool"
+              src="/images/la-era-pool.jpg"
+              alt="pool-img"
+            />
+          </Carousel.Item>
+
       </Carousel>
-      <img src="/images/background-design.png" className="background-right-side"/>
     </div>
   );
 }
